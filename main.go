80588a6c9e8d53dbec35e541ebe345b73d70769c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-07-28 20:41:10 -0300

    Removendo código morto. (Parte 2 da aula 4 do curso 'Go e Gin: criando API rest com simplicidade')

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/Henriquemcc/api-go-gin/database"
-	"github.com/Henriquemcc/api-go-gin/models"
 	"github.com/Henriquemcc/api-go-gin/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "João da Silva", CPF: "893.754.630-28", RG: "44.145.525-6"},
-		{Nome: "José da Silva", CPF: "547.444.120-76", RG: "36.602.077-8"},
-	}
 	routes.HandleRequests()
 }
